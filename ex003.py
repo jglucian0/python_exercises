fe23338807452071142be34552abd003c52db8a5
João Gabriel <jgluciano.luz@gmail.com>
2024-12-10 00:58:36 -0300

    Add new ex002.py and ex003.py

---
+++ ex003.py
@@ -0,0 +1,5 @@
+# Crie um programa que leia dois números e mostre a soma entre eles.
+numberA = int(input("Digite o primeiro número: "))
+numberB = int(input("Digite o segundo número: "))
+
+somaAB = input("O resultado da soma é {}!".format(numberA + numberB))
